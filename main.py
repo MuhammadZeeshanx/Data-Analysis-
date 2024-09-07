6e94b9e34ac6b140ddfdd9361fa5c4c7b358e58f
Muhammad Zeeshan <148358668+MuhammadZeeshanx@users.noreply.github.com>
2024-09-07 00:09:38 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# pip install numpy
+# pip install pandas
+# pip install matplotlib
+# pip install seaborn
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as pt
+import seaborn as sns
+
+df = pd.read_csv("")
+print ()
